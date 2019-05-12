1f94e9a89b54afe7bb9f1fabcead9d3496cacfa9
Josue <j-david-10@hotmail.com>
2019-05-12 13:28:01 -0600

    img uploaded

--- train.py
+++ train.py
@@ -2,6 +2,9 @@ from math import exp
 from random import seed
 from random import random
 import pickle
+from PIL import Image
+import glob
+import cv2
 
 # Initialize a network
 def initialize_network(n_inputs, n_hidden, n_outputs):
@@ -84,16 +87,13 @@ def train_network(network, train, l_rate, n_epoch, n_outputs):
 
 # Test training backprop algorithm
 seed(1)
-dataset = [[2.7810836,2.550537003,0],
-	[1.465489372,2.362125076,0],
-	[3.396561688,4.400293529,0],
-	[1.38807019,1.850220317,0],
-	[3.06407232,3.005305973,0],
-	[7.627531214,2.759262235,1],
-	[5.332441248,2.088626775,1],
-	[6.922596716,1.77106367,1],
-	[8.675418651,-0.242068655,1],
-	[7.673756466,3.508563011,1]]
+
+files = glob.glob ("D:/Documents/Semestres/7/IA/Examen1/Backpropagation-images/bmpfiles/*.bmp") # your image path
+dataset = []
+for myFile in files:
+    image = cv2.imread (myFile)
+    dataset.append([image, 0])
+print(dataset)
 n_inputs = len(dataset[0]) - 1
 n_outputs = len(set([row[-1] for row in dataset]))
 network = initialize_network(n_inputs, 2, n_outputs)
